cec973bb095c430765fdd0e8040d8a29f5a9dd93
Bayu Prasetya Utomo <108232231+BayuPrasetyaUtomo@users.noreply.github.com>
2024-10-23 21:05:44 +0700

    feat: simple calculations with layout

--- calculator.js
+++ calculator.js
@@ -1,22 +1,52 @@
-const add = (...num) => {
-  return num.reduce((acc, curr) => acc + curr, 0)
-};
+const add = (num) => Number(num.reduce((acc, curr) => acc + curr, 0).toFixed(2))
 
-const subtract = (...num) => {
-  return num.reduce((acc, curr) => acc - curr, 0)
+const subtract = (num) => Number(num.reduce((acc, curr) => acc - curr).toFixed(2))
+
+const multiply = (num) => Number(num.reduce((acc, curr) => acc * curr).toFixed(2))
+
+const divide = (num) => Number(num.reduce((acc, curr) => acc / curr).toFixed(2))
+
+const buttons = document.querySelector("#calculator .buttons")
+const input = document.querySelector("input")
+
+const numButtons = [7, 8, 9, "Del", 4, 5, 6, "*", 1, 2, 3, "/", 0, "+", "-", "="]
+const operators = ["+", "-", "*", "/", "Del", "="]
+
+const isOperator = (value) => operators.find(operator => value === operator)
+
+let op = "";
+const showCalculator = () => {
+
+  numButtons.forEach((item) => {
+    const button = document.createElement("button");
+    button.textContent = item;
+    if (operators.includes(item)) { button.setAttribute("style", "background-color: rgb(3 105 161);color: var(--foreground)") }
+    button.addEventListener("click", () => getInputValue(item.toString()));
+    buttons.appendChild(button);
+  })
 }
 
-const multiply = (...num) => {
-  return num.reduce((acc, curr) => acc * curr)
+
+// Check the value
+const getInputValue = (value) => {
+  if (typeof parseInt(value) !== NaN && value !== "=") {
+    input.value += value
+  }
+
+  // if value is an mathematic operator
+  if (isOperator(value)) {
+    if (value === "=") {
+      const nums = input.value.split(op).map(num => parseInt(num))
+
+      if (op === "+") input.value = add(nums)
+      else if (op === "-") input.value = subtract(nums)
+      else if (op === "*") input.value = multiply(nums)
+      else if (op === "/") input.value = divide(nums)
+    }
+    else op = value;
+  }
+
+  if (value.toLowerCase() === "del") input.value = "";
 }
 
-const divide = (...num) => {
-  return num.reduce((acc, curr) => acc / curr)
-}
-
-const run = (a, op, b) => {
-  if (op === "+") return add(a, b);
-  else if (op === "-") return subtract(a, b);
-  else if (op === "*") return multiply(a, b);
-  else if (op === "/") return divide(a, b);
-}
+showCalculator()
